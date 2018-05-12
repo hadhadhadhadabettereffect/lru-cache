5e43779a19ce7021428981b07747f9b187da1960
Eric Birkeland <ericjbirkeland@gmail.com>
2018-05-12 18:53:24 -0400

    added history tests

--- tests.js
+++ tests.js
@@ -1,12 +1,18 @@
-const test = require("tape");
-const tapSpec = require("tap-spec");
-const LRUCache = require("./LRUCache");
+var test = require("tape");
+var tapSpec = require("tap-spec");
+var LRUCache = require("./LRUCache");
 
 test.createStream()
     .pipe(tapSpec())
     .pipe(process.stdout);
 
-test("capacity property value", function (t) {
+test("LRUCache tests", function (t) {
+    t.plan(2);
+    t.test("capacity property value", capacityTests);
+    t.test("history tests", historyTests);
+});
+
+function capacityTests(t) {
     t.plan(9);
     var cache = new LRUCache();
 
@@ -38,11 +44,39 @@ test("capacity property value", function (t) {
         "if capacity is set with an invalid number, it will be set to 1");
 
     cache.capacity = 5;
-    for (let i = 0; i < 10; cache.put("k"+i, i++));
+    for (var i = 0; i < 10; cache.put("k" + i, i++));
     t.equal(cache.items.size, 5,
         "lruCache objects will not store values beyond their capacities");
 
     cache.capacity = 2;
     t.equal(cache.items.size, 2,
         "the number of stored values reduced when capacity is lowered");
-});
+}
+
+function historyTests(t) {
+    t.plan(7);
+    var cap = 4;
+    var cache = new LRUCache(cap);
+
+    for (var i = 0; i < cap; cache.put(`${i}`, i++));
+    t.equal(cache.rootNode.next.key, "0",
+        "item closest to root node should be least recently used");
+
+    cache.put(`${cap}`, cap);
+    t.false(cache.items.has("0"),
+        "least recently used item will be removed when capacity exceeded");
+    t.equal(cache.rootNode.next.key, "1",
+        "the item after the previous lru will become the lru after removal");
+    t.equal(cache.tailNode.prev.key, `${cap}`,
+        "newly added items are set as most recent");
+
+    cache.get("1");
+    t.equal(cache.tailNode.prev.key, "1",
+        "cache.get(key) sets key as most recent");
+    t.equal(cache.rootNode.next.key, "2",
+        "if an item is moved to be most recent, items once after it shift down");
+
+    cache.put("3", 0);
+    t.equal(cache.tailNode.prev.key, "3",
+        "cache.put(key) sets key as most recent");
+}
